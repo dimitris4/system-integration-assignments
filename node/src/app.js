1a7a8f664acf7e0440c7202c8a67d58cf432de7a
Dimitrios Gkiokas <dimitris.giokas1@gmail.com>
2022-08-31 00:30:03 +0200

    es6 modules

---
+++ node/src/app.js
@@ -0,0 +1,16 @@
+import { readFileSync } from 'fs';
+
+console.log('\n.txt')
+function syncReadFile(filename) {
+    const contents = readFileSync(filename, 'utf-8');
+
+    const arr = contents.split(/\r?\n/);
+
+    console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
+
+    return arr;
+}
+
+syncReadFile('../words.txt');
+
+
